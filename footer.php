f9a9e9ce1df56318dac0472abaad2c9e912b7399
saeb molaee <saeb.m.13@gmail.com>
2017-03-11 00:34:54 +0330

    first init

---
+++ footer.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains all content after the main content area and sidebar
+ *
+ * @package Gambit
+ */
+
+?>
+
+		</div><!-- #content -->
+
+	</div><!-- #content-wrap -->
+
+	<?php do_action( 'gambit_before_footer' ); ?>
+
+	<div id="footer" class="site-footer-wrap">
+
+		<footer id="colophon" class="site-footer container clearfix" role="contentinfo">
+
+			<div id="footer-text" class="site-info">
+				
+
+	<?php	bloginfo(); ?>
+	&copy;	
+&nbsp;
+<?php the_date();
+//do_action( 'gambit_footer_text' ); 
+
+?>
+&nbsp;
+
+			</div><!-- .site-info -->
+
+			<?php do_action( 'gambit_footer_menu' ); ?>
+
+		</footer><!-- #colophon -->
+
+	</div>
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,19 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+function my_theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+?>
+<?php
+/**
+ * Setup My Child Theme's textdomain.
+ *
+ * Declare textdomain for this child theme.
+ * Translations can be filed in the /languages/ directory.
+ */
+function my_child_theme_setup() {
+    load_child_theme_textdomain( 'gambit', get_stylesheet_directory() . '/languages' );
+}
+add_action( 'after_setup_theme', 'my_child_theme_setup' );
+?>
